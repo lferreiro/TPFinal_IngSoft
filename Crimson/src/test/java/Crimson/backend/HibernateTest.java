19a5dee76ae4f2adbc39f1183cfd4b0754ffdcaa
Luciano <luciano.ferreiro95@gmail.com>
2019-06-12 15:47:01 -0300

    cambios en hibernate cfg create-drop

--- Crimson/src/test/java/Crimson/backend/HibernateTest.java
+++ Crimson/src/test/java/Crimson/backend/HibernateTest.java
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class HibernateTest {
     private Asiento asiento;
