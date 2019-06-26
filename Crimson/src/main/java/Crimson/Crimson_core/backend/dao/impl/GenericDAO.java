b50db3a53f84b79ae3fe77ca50062f110c6e5d7f
miguelbada <miguelenriquebada07@gmail.com>
2019-06-25 22:01:47 -0300

    reservaDao y usuarioDao

--- Crimson/src/main/java/Crimson/Crimson_core/backend/dao/impl/GenericDAO.java
+++ Crimson/src/main/java/Crimson/Crimson_core/backend/dao/impl/GenericDAO.java
@@ -6,4 +6,8 @@ public interface GenericDAO<T> {
     void add(T entity);
 
     T get(Serializable id);
+
+    void update(T entity);
+
+    void delete(T entity);
 }
--- Crimson/src/test/java/Crimson/Crimson_core/backend/HibernateTest.java
+++ Crimson/src/test/java/Crimson/Crimson_core/backend/HibernateTest.java
@@ -1,7 +1,6 @@
 package Crimson.Crimson_core.backend;
 
 import Crimson.Crimson_core.*;
-import Crimson.Crimson_core.backend.dao.impl.HibernateGenericDAO;
 import Crimson.Crimson_core.backend.dao.impl.ReservaDAO;
 import Crimson.Crimson_core.backend.dao.impl.UsuarioDAO;
 import Crimson.Crimson_core.backend.service.TransactionRunner;
@@ -14,21 +13,25 @@ import static org.junit.Assert.*;
 public class HibernateTest {
     private UsuarioDAO usuarioDAO;
     private ReservaDAO reservaDAO;
-    private Sala sala;
+    private Sala sala1;
+    private Sala sala2;
     private Usuario usuario1;
     private Usuario usuario2;
-    private Pelicula pelicula;
+    private Pelicula pelicula1;
+    private Pelicula pelicula2;
     private Cartelera cartelera;
 
     @Before
     public void setUp() throws Exception {
         usuarioDAO = new UsuarioDAO();
         reservaDAO = new ReservaDAO();
-        sala = new Sala(30, 1, "2D");
+        sala1 = new Sala(30, 1, "2D");
+        sala2 = new Sala(20, 2, "3D");
         cartelera = new Cartelera();
         usuario1 = new Usuario("Miguel", 2566464, "Miguel@gmail.com");
         usuario2 = new Usuario("Juan", 45678, "juan@gmail.com");
-        pelicula = new Pelicula("Scott Pilgrim vs The World", "Comedia", "+13", "", sala, cartelera );
+        pelicula1 = new Pelicula("Scott Pilgrim vs The World", "Comedia", "+13", "", sala1, cartelera );
+        pelicula2 = new Pelicula("Scott Pilgrim vs The World", "Comedia", "+13", "", sala2, cartelera );
     }
 
     @Test
@@ -48,14 +51,18 @@ public class HibernateTest {
     @Test
     public void recuperarReserva() {
         TransactionRunner.runInSession(() -> {
-            usuario2.generarReserva(5, pelicula);
+            usuario2.generarReserva(5, pelicula1);
+            usuario2.generarReserva(2, pelicula1);
             usuarioDAO.add(usuario2);
 
-            Reserva reservaGet = reservaDAO.get(1L);
+            long reservaId = usuario2.getReservas().get(0).getId();
+
+            Reserva reservaGet = reservaDAO.get(reservaId);
 
             assertEquals(usuario2.getReservas().get(0), reservaGet);
 
             return null;
         });
     }
+
 }
