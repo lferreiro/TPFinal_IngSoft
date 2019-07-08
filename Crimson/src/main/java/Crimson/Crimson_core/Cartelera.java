4e67fe1ae8d76533939083ab7bc5faa3506bde7e
Luciano <luchisonico@gmail.com>
2019-07-08 12:47:10 -0300

    Application Startup
    • Los componentes del REST ahora tienen un constructor vacio y un setter para sus dependencias.
    • Creo archivo Beans.xml para mapear beans con clases.
    • Ahora Application no es automatica y se encarga de hacer uso del context para instanciar e inicializar beans.
    Queda pendiente levantar el sevicio REST usando componentes como figuran [aca](https://www.baeldung.com/spring-controllers) para que la aplicacion escuche pedidos.

--- Crimson/src/main/java/Crimson/Crimson_core/Cartelera.java
+++ Crimson/src/main/java/Crimson/Crimson_core/Cartelera.java
@@ -3,6 +3,7 @@ package Crimson.Crimson_core;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -20,7 +21,9 @@ public class Cartelera {
         this.peliculaList = peliculas;
     }
 
-    public Cartelera() {}
+    public Cartelera() {
+        this.peliculaList = new ArrayList<>();
+    }
 
     public void agregarPelicula(Pelicula pelicula){
         this.peliculaList.add(pelicula);
--- Crimson/src/main/java/Crimson/Crimson_core/RestApp/DataManager.java
+++ Crimson/src/main/java/Crimson/Crimson_core/RestApp/DataManager.java
@@ -12,8 +12,8 @@ public class DataManager {
 
     public DataManager() {}
 
-//    public DataManager(Cartelera cartelera) {
-//        this.cartelera = cartelera;
-//    }
+    public void setCartelera(Cartelera cartelera) {
+        this.cartelera = cartelera;
+    }
 
 }
