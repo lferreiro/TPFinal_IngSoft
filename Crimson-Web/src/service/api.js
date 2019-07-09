f16bd6f66b77157a7b72176950261e3fbf19c845
Luciano <luchisonico@gmail.com>
2019-07-09 18:09:57 -0300

    Nuevos campos Preview y Detalle
    • Cambio los datos mock de api.js por unos nuevos que imitan a los del futuro getDatosPelicula().
    • Cambio el mock en Home.
    • Adapto los campos en DetallePelicula a los que traerá getDatosPelicula() y agrego el embedded de Youtube para el trailer.

--- Crimson-Web/src/service/api.js
+++ Crimson-Web/src/service/api.js
@@ -10,22 +10,29 @@ const API = {
 
 export default API;
 export const fakeDatos = {
-  pelicula: {
-    nombre: 'Aladdin',
-    codigo: 1,
-    genero: 'Aventura Romantica',
-    clasificacion: 'ATP',
-    sinopsis: 'Aladdin (Mena Massoud) es un adorable pero desafortunado ladronzuelo enamorado de la hija del Sultán, la princesa Jasmine (Naomi Scott). Para intentar conquistarla, acepta el desafío de Jafar (Marwan Kenzari), que consiste en entrar a una cueva en mitad del desierto para dar con una lámpara mágica que le concederá todos sus deseos. Allí es donde Aladdín conocerá al Genio (Will Smith), dando inicio a una aventura como nunca antes había imaginado',
-    sala: {
-      numeroSala: 3,
-      asientosSala: null,
-      cantidadAsientos: 30,
-      asientosOcupados: 0,
-      tipoSala: '2D',
+  nombre: 'Aladdin',
+  genero: 'Aventura Romantica',
+  clasificacion: 'ATP',
+  sinopsis: 'Aladdin (Mena Massoud) es un adorable pero desafortunado ladronzuelo enamorado de la hija del Sultán, la princesa Jasmine (Naomi Scott). Para intentar conquistarla, acepta el desafío de Jafar (Marwan Kenzari), que consiste en entrar a una cueva en mitad del desierto para dar con una lámpara mágica que le concederá todos sus deseos. Allí es donde Aladdín conocerá al Genio (Will Smith), dando inicio a una aventura como nunca antes había imaginado',
+  trailer: 'https://www.youtube.com/watch?v=q84t9ORBf40',
+  funciones: [
+    {
+      sala: {
+        numero: 5,
+        cantAsientos: 30,
+        asientosOcupados: 25,
+        tipo: '2D',
+      },
+      date: '15-05-17 19:30:00',
     },
-  },
-  info: {
-    vista: true,
-    reservada: false,
-  },
+    {
+      sala: {
+        numero: 5,
+        cantAsientos: 30,
+        asientosOcupados: 15,
+        tipo: '2D',
+      },
+      date: '17-08-17 22:00:00',
+    },
+  ],
 };
